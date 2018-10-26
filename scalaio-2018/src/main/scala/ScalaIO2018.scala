e88e224d022a92d9a0c81949c53a8a06463cdcec
paul-heymann <paul.heymann@xing.com>
2018-10-26 15:47:07 +0200

    updated slides (content, style)

--- scalaio-2018/src/main/scala/ScalaIO2018.scala
+++ scalaio-2018/src/main/scala/ScalaIO2018.scala
@@ -27,40 +27,52 @@ object ScalaIO extends JSApp {
     ),
 
     slide(
-      "Type-Level Programming: hands-on example",
-      <.p("How can we convince the compiler to derive HTTP client functions from an API descriptions?"),
-      <.br,
+      "Type-Level Programming: What the compiler has to figure out",
       scalaC("""
         // GET /users/:name?minAge=:age -> List[User]
+
+                          \/
 
         (name: String, minAge: Int) => F[List[User]]
       """)
     ),
 
     slide(
-      "What we need",
-      <.h2("1"),
-      <.p("represent information on the type-level using singleton types"),
+      "Type-Level Programming: basics",
+      <.img(
+        ^.src   := "./img/one.svg",
+        ^.width := "7%"
+      ),
+      <.br,
+      <.br,
+      <.p("Represent information on the type-level using singleton types."),
     ),
 
     slide(
-      "What we need",
-      <.h2("2"),
+      "Type-Level Programming: basics",
+      <.img(
+        ^.src   := "./img/two.svg",
+        ^.width := "8%"
+      ),
+      <.br,
+      <.br,
       <.p("implicit recursion ~ type-level computation")
     ),
 
     noHeaderSlide(
-      <.p("????")
+      <.img(
+        ^.src   := "./img/lookat.jpg",
+        ^.width := "80%",
+      )
     ),
 
     slide(
-      "Speaker",
-      <.h3("Paul Heymann - Data Engineer @ XING")
-    ),
-
-    noHeaderSlide(
-      <.h2("You have a question?"),
-      <.h3("Ask it right away!")
+      "Me: Paul Heymann",
+      Enumeration(
+        Item.stable("Data Engineer @ Xing"),
+        Item.fadeIn("Scala Trainer"),
+        Item.fadeIn("infrequently speaking at conferences and Meetups")
+      )
     )
   )
 
@@ -68,42 +80,37 @@ object ScalaIO extends JSApp {
     chapterSlide(
       <.h2("API as a Type"),
       <.br,
-      <.h3("How to represent information on the Type-Level")
+      <.p("How to represent information on the Type-Level")
     ),
 
     slide(
-      "Our Task",
-      <.p("Represent the following API as a Type:"),
-      <.br,
+      "API as a Type",
       <.p(font(^.color := "#919191", "GET /users/:name?minAge=:age -> List[User]"))
     ),
 
     slide(
-      "Path to Type",
+      "Path as a Literal-Type",
       <.p(font(^.color := "#919191", "GET /"), font(^.color := "#ce1d25", "users"), font(^.color := "#919191", "/:name?minAge=:age -> List[User]")),      
       <.br,
       scalaC("""
+        // not in vanilla Scala 2.12
         type Users = "users".type
-      """),
-      scalaCFragment(
-        "// Vanilla Scala (2.12) has no accessable literal singleton types (yet). That's a bummer."
-      )
+      """)
     ),
 
-    noHeaderSlide(
-      <.p("shapeless.Witness"),
-      <.br,
+    slide(
+      "Literal-Types with Witness",
       scalaC("""
         trait Witness {
           type T
 
-          val value: T
+          val value: T {}
         }
       """)
     ),
 
     slide(
-      "Witness as Literal Singleton Type",
+      "Literal-Types with Witness",
       scalaC("""
         val usersW = Witness("users")
 
@@ -113,8 +120,6 @@ object ScalaIO extends JSApp {
 
     slide(
       "Make it more precise",
-      <.p("Now we have a type. Let's make it a bit more precise."),
-      <.br,
       scalaC("""
         sealed trait Path[P]
 
@@ -125,9 +130,9 @@ object ScalaIO extends JSApp {
     noHeaderSlide(
       <.p("That's basically it. Now you know how to get information onto the Type-Level."),
       <.br,
-      <.h3(
+      <.h4(
         Style("textTransform") := "none",
-        "Singleton Types"
+        "Singleton-Types"
       )
     ),
 
@@ -168,7 +173,8 @@ object ScalaIO extends JSApp {
       ),
       <.br,
       scalaC("""
-        sealed trait Get[O]
+        sealed trait Method
+        sealed trait Get[O] extends Method
 
         type GetUsers = Get[List[User]]
       """)
@@ -184,7 +190,7 @@ object ScalaIO extends JSApp {
     ),
 
     slide(
-      "Make it a Whole",
+      "Create a complete API",
       <.p(
         font(^.color := "#919191", "GET /users/:name?minAge=:age -> List[User]")
       ),
@@ -199,7 +205,7 @@ object ScalaIO extends JSApp {
     ),
 
     slide(
-      "Make it convinient",
+      "Create a complete API: v2.0",
       scalaC("""
         val apiT = api(
           method  = Get[List[Users]],
@@ -212,35 +218,39 @@ object ScalaIO extends JSApp {
     ),
 
     slide(
-      "Methods",
-      <.p("We made Get a type without inhabitants. How can we still pass it to a function?"),
-      <.br,
+      "How to carry a Method-Type",
       scalaC("""
         final case class TypeCarrier[T]()
-      """),      
-      scalaCFragment("""
+
         def Get[O] = TypeCarrier[Get[O]]()
       """)
     ),
 
     slide(
-      "Paths and Segments",
-      <.p("You can use implicitly transformation to derive Witnesses for paths and segments."),
-      <.br,
+      "Build up the Paths",
       scalaC("""
         final case class PathBuilder[P <: HList]() {
   
+          // implicit conversion
           def /[S](path: Witness.Lt[S]): PathBuilder[S :: P] = 
             PathBuilder()
+          
+          ...
         }
+
+        def Root = PathBuilder[HNil]
+      """),
+      scalaCFragment("""
+        Root / "users"
       """)
     ),
 
     slide(
-      "Paths and Segments",
-      <.p("We need some help to get segment types."),
-      <.br,
+      "Build up the Path: Segment",
       scalaC("""
+        val nameT = Segment[String]("name")
+      """),
+      scalaCFragment("""
         class SegmentHelper[V] {
 
           def apply[K](kWit: Witness.Lt[K]): TypeCarrier[Segment[K, V]] = 
@@ -248,32 +258,33 @@ object ScalaIO extends JSApp {
         }
 
         def Segment[V] = new SegmentHelper[V]
-      """),
-      scalaCFragment("""
-        val nameT = Segment[String]("name")
       """)
     ),
 
     slide(
-      "Paths and Segments",
+      "Build up the Path: Segment",
       scalaC("""
         final case class PathBuilder[P <: HList]() {
-  
           ...
-          def /[K, V](segment: TypeCarrier[SegmentParam[K, V]]): PathBuilder[SegmentParam[K, V] :: P] = 
+
+          def /[K, V](segment: TypeCarrier[SegmentParam[K, V]]): 
+              PathBuilder[SegmentParam[K, V] :: P] = 
             PathListBuilder()
         }
-
-        val Root = PathBuilder[HNil]()
+      """),
+      scalaCFragment("""
+        Root / "users" / Segment[String]("name")
       """)
     ),
 
     slide(
-      "Make it convinient",
+      "Create a complete API: v2.0",
       scalaC("""
-        def api[M, P <: HList, Q <: HList, Api <: HList]
-          (method: M, path: PathBuilder[P], queries: QueryBuilder[Q])
-          (implicit prepQP: Prepend.Aux[Q, P, Api]): TypeCarrier[M :: Api] = TypeCarrier()
+        def api[M <: Method, P <: HList, Q <: HList, Api <: HList]
+            (method: M, path: PathBuilder[P], queries: QueryBuilder[Q])
+            (implicit prepQP: Prepend.Aux[Q, P, Api], rev: Reverse[Api]): 
+            TypeCarrier[M :: rev.Out] = 
+          TypeCarrier()
       """),
       scalaCFragment("""
 
@@ -283,10 +294,6 @@ object ScalaIO extends JSApp {
           queries = Queries add Query[Int]("minAge")
         )
       """)
-    ),
-
-    noHeaderSlide(
-      <.p("We got our information onto the Type-Level. Now let's derive the client.")
     )
   )
 
@@ -294,40 +301,53 @@ object ScalaIO extends JSApp {
     chapterSlide(
       <.h2("Derive Clients"),
       <.br,
-      <.h3("Recursively derive a client function.")
+      <.p("Recursively derive a client function.")
     ),
 
     slide(
-      "Current State",
-      <.p("What we want to end up with:"),
-      <.br,
+      "What's next - derive the client",
       scalaC("""
-        val client: (String, Int) => IO[List[User]] = (name, minAge) => ???
+        val client = derive(Api)
+ 
+        // => client: (String, Int) => IO[List[User]]
       """)
     ),
 
     noHeaderSlide(
-      <.p("Ho does the HTTP request looks like.")
+      <.p("What information do we need?")
     ),
 
     slide(
-      "Do a Request",
+      "Necessary information",
       scalaC("""
-        trait HttpRequest[M, F[_], C, Out] {
-
-          def apply(path: List[String], queries, Map[String, List[String]), client: C): F[Out]
-        }
+        val client = derive[Api]
+ 
+        // => client: (String, Int) => IO[List[User]]
       """),
-      scalaCFragment("""
+      Enumeration(
+        Item.stable("types of input parameters (segment, query)"),
+        Item.stable("return type")
+      )
+    ),
 
-        implicit def http4sIOGet[O] = new HttpRequest[GetMethod, IO, Client[IO], O] {
-          ...
+    noHeaderSlide(
+      <.p("But that is not enough to describe a request.")
+    ),
+
+    slide(
+      "I am a side-effect",
+      scalaC("""
+        trait HttpRequest[M <: Method, F[_], C, Out] {
+
+          def apply(path: List[String], 
+                    queries, Map[String, List[String]), client: C): 
+            F[Out]
         }
       """)
     ),
 
     slide(
-      "What we need",
+      "Necessary information",
       Enumeration(
         Item.stable("types of input parameters (segment, query)"),
         Item.fadeIn("literal types of queries"),
@@ -337,11 +357,9 @@ object ScalaIO extends JSApp {
     ),
 
     slide(
-      "What we need",
-      <.p("What we want is to fold our API type."),
-      <.br,
+      "Extract that information",
       scalaC("""
-        Get[List[User]] :: MinAgeQ :: Name :: Users :: HNil 
+        Get[List[User]] :: Users :: Name :: MinAgeQ :: HNil 
 
             ~>
 
@@ -356,20 +374,16 @@ object ScalaIO extends JSApp {
     ),
 
     noHeaderSlide(
-      <.p("But how do you fold a HList type?")
-    ),
-
-    noHeaderSlide(
-      <.p("How to iterate over a HList type?")
+      <.p("But how do you transform a HList type?")
     ),
 
     slide(
-      "Iterate over a HList type",
+      "Iterating over a HList type",
       <.p("You can leverage Scala's implicit resolution mechanism to iterate over an HList type. Use implicit recursive resolution.")
     ),
 
     slide(
-      "Type-Level Fold Left",
+      "Type-Level fold",
       scalaC("""
         // start with a type-class describing your computation
         trait TplFolder[H <: HList, Agg] { type Out }
@@ -378,58 +392,57 @@ object ScalaIO extends JSApp {
       """),
       scalaCFragment("""
         // final case
-        implicit def returnTplFolder[Agg] = new TypeLevelFoldLeft[HNil, Agg] {
+        implicit def returnTplFolder[Agg] = new TplFolder[HNil, Agg] {
           type Out = Agg
         }
       """),
       scalaCFragment("""
         // recursive resolution (useless for now)
-        implicit def recTplFolder[H, T <: HList, Agg, FOut](implicit next: TplFolder.Aux[T, H, FOut]) =
+        implicit def recTplFolder[H, T <: HList, Agg, FOut]
+            (implicit next: TplFolder.Aux[T, H, FOut]) =
           new TplFolder[H :: T, Agg] { type Out = FOut }
       """)
     ),
 
     slide(
-      "Type-Level Fold Left",
-      <.p("Define a mapping case from two types In and Agg to Out."),
-      <.br,
+      "Type-Level fold: case",
       scalaC("""
         trait TplCase[In, Agg] { type Out }
 
         type Aux[In, Agg, Out0] = TplCase[In, Agg] { type Out = Out0 }
       """),
       scalaCFragment("""
+        implicit def getCase[Out] =
+          new TplCase[Get[Out], Unit] {
+            type Out = (HNil, HNil, HNil, GetCall, Out)
+          }
+      """)
+    ),
+
+    slide(
+      "Type-Level fold: query",
+      scalaC("""
         implicit def queryCase
             [K <: Symbol, V, El <: HList, KIn <: HList, VIn <: HList, M, Out] =
           new TplCase[Query[K, V], (El, KIn, VIn, M, Out)] {
             type Out = (QueryInput :: El, K :: KIn, V :: VIn, M, Out)
           }
- 
-        ...
       """)
     ),
 
     slide(
-      "Type-Level Fold Left",
-      <.p("Define a mapping case from two types In and Agg to Out."),
-      <.br,
+      "Type-Level fold: query",
       scalaC("""
-        trait TplCase[In, Agg] { type Out }
-
-
-
         //implicit def queryCase
         //    [K <: Symbol, V, El <: HList, KIn <: HList, VIn <: HList, M, Out] =
           new TplCase[Query[K, V], (El, KIn, VIn, M, Out)] {
             type Out = (QueryInput :: El, K :: KIn, V :: VIn, M, Out)
           }
- 
-        ...
       """)
     ),
 
     slide(
-      "Type-Level Fold Left",
+      "Type-Level fold",
       scalaC("""
         // recursive resolution (useleful)
         implicit def recTplFolder[H, T <: HList, Agg, COut, FOut]
@@ -442,7 +455,10 @@ object ScalaIO extends JSApp {
     slide(
       "Fold our API",
       scalaC("""
-        type Api = Get[List[User]] :: MinAgeQ :: Name :: Users :: HNil
+        implicitly[TplFolder[
+          Get[List[User]] :: MinAgeQ :: Name :: Users :: HNil, 
+          Unit
+        ]]
 
         // (HNil, HNil, HNil, GetCall, List[User])
         recTplFolder(getCase,
@@ -454,7 +470,179 @@ object ScalaIO extends JSApp {
               recTplFolder(pathCase,
                 returnCase))))
       """)
-    )
+    ),
+
+    noHeaderSlide(
+      <.p("We have the information we need. Now we will collect all data needed to do a request.")
+    ),
+
+    slide(
+      "Derive a function to collect request data",
+      scalaC("""
+        type PathB   = List[String]
+        type Queries = Map[String, List[String]]
+
+        trait RequestDataBuilder[El <: HList, 
+                                 KIn <: HList, 
+                                 VIn <: HList] {
+
+          def apply(inputs: VIn,
+                    path: PathB,
+                    queries: Queries): (PathB, Queries)
+        }
+      """)
+    ),
+
+    slide(
+      "Final implicit case",
+      scalaC("""
+        implicit val returnBuilder = 
+          new RequestDataBuilder[HNil, HNil, HNil] {
+
+            def apply(inputs: HNil, 
+                      path: PathB, 
+                      queries: Queries): (PathB, Queries) =
+              path -> queries
+          }
+      """)
+    ),
+
+    slide(
+      "Extract query parameter",
+      scalaC("""
+        implicit def queryBuilder
+            [K <: Symbol, V, T <: HList, KIn <: HList, VIn <: HList, M, O]
+            (implicit wit:  Witness.Aux[K], 
+                      next: RequestDataBuilder[T, KIn, VIn]) =
+          new RequestDataBuilder[QueryInput :: T, K :: KIn, V :: VIn] {
+
+            def apply(inputs: V :: VIn, 
+                      path: PathB, 
+                      queries: Queries): (PathB, Queries) =
+              next(
+                inputs.tail,
+                path,
+                queries + (wit.value.name -> List(inputs.head.toString()))
+              )
+          }
+      """)
+    ),
+
+    slide(
+      "Extract query parameter",
+      scalaC("""
+        //implicit def queryBuilder
+        //    [K <: Symbol, V, T <: HList, KIn <: HList, VIn <: HList, M, O]
+        //    (implicit wit:  Witness.Aux[K], 
+        //              next: RequestDataBuilder[T, KIn, VIn]) =
+          new RequestDataBuilder[QueryInput :: T, K :: KIn, V :: VIn] {
+
+            def apply(inputs: V :: VIn, 
+                      path: PathB, 
+                      queries: Queries): (PathB, Queries) =
+              next(
+                inputs.tail,
+                path,
+                queries + (wit.value.name -> List(inputs.head.toString()))
+              )
+          }
+      """)
+    ),
+
+    slide(
+      "Derive a function to collect request data",
+      scalaC("""
+        val builder = implicitly[RequestDataBuilder[El, KIn, VIn]]
+
+        builder("Gandalf" :: 2019 :: HNil, Nil, Map())
+          -> next(input, "users" :: path, queries)
+            -> next(input.tail, "Gandalf" :: path , queries)
+              -> next(input.tail, path, queries + ("minAge" -> List("2019")))
+                -> (List("users", "Gandalf"), Map("minAge" -> List("2019")))
+      """)
+    ),
+
+    noHeaderSlide(
+      <.p("Put it all together and we have a HTTP client.")
+    ),
+
+    slide(
+      "Complete client",
+      scalaC("""
+        val req = implicitly[
+          HttpRequest[GetMethod, IO, Client[IO], List[User]]
+        ]
+
+        val client: (String, Int) => IO[List[User]] = (name, age) => {
+          val (path, queries) = builder(name :: age :: HNil, Nil, Map())
+
+          req(path, queries, c)
+        }
+      """)
+    ),
+
+    slide(
+      "Derive HTTP client function",
+      scalaC("""
+        def derive
+            [Api <: HList, El <: HList, KIn <: HList, VIn <: HList, M, Out, C]
+            (api: TypeCarrier[Api])
+            (implicit fold:    Lazy[TypeLevelFoldLeft.Aux[Api, Fold], 
+                                    (El, KIn, VIn, M, Out)]
+                      builder: RequestBuilder[El, KIn, VIn],
+                      req:     ApiRequest[M, IO, C, Out])
+            : VIn => C => IO[Out] = 
+          vin => c => {
+            val (path, queries) = builder(vin, Nil, Map())
+
+            req(path, queries, c)
+          }
+      """)
+    ),
+
+    slide(
+      "HTTP client function",
+      scalaC("""
+        val client = derive(Api)
+
+        // we have to provide a HList
+        client("Gandalf" :: 2019 :: HNil)(c)
+      """)
+    ),
+
+    slide(
+      "Derive HTTP client function: v2.0",
+      scalaC("""
+        def derive
+        //    [Api <: HList, El <: HList, KIn <: HList, VIn <: HList, M, Out, C]
+        //    (api: TypeCarrier[Api])
+        //    (implicit fold:    Lazy[TypeLevelFoldLeft.Aux[Api, Fold], 
+        //                            (El, KIn, VIn, M, Out)]
+        //              builder: RequestBuilder[El, KIn, VIn],
+        //              req:     ApiRequest[M, IO, C, Out],
+                      vinToFn: FnFromProduct[VIn => C => IO[Out]])
+            : vinToFn.Out = vinToFn { input => c =>
+          val (path, queries) = builder(input, Nil, Map())
+
+          req(path, queries, c)
+        }
+      """)
+    ),
+
+    slide(
+      "Final result",
+      scalaC("""
+        val apiT = api(
+          method  = Get[List[Users]],
+          path    = Root / "users" / Segment[String]("name"),
+          queries = Queries add Query[Int]("minAge")
+        )
+
+        val client = derive(Api)
+
+        client("Gandalf", 2019)(c)
+      """)
+    ),
   )
 
   val Show = ScalaComponent
